6d1fc60aaf006154c79080aec55c569cf2bfa3d1
Artem Kornev <artemkakornev1@gmail.com>
2024-03-01 21:01:22 +0200

    Update TypeScript SDK path and add data bean

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ import { Bean, ClawjectApplication } from "@clawject/di";
 
 @ClawjectApplication
 class ApplicationContext {
-    
+    @Bean data(
+        n: number
+    ) {
+        return 123;
+    }
 }
